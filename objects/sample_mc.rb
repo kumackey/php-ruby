8c0fb8d46ba790cdfff3f76fb003714819c4cacd
kumackey <kumak1t09e0@gmail.com>
2020-12-01 08:43:34 +0900

    first commit

---
+++ objects/sample_mc.rb
@@ -0,0 +1,6 @@
+string3 = 3
+puts string3.to_s
+# 3
+
+puts string3.to_s.class
+# String
---
+++ objects/sample_om.php
@@ -0,0 +1,7 @@
+<?php
+$string3 = 3;
+echo intval($string3);
+// 3
+
+echo get_class(intval($string3));
+// Warning: get_class() expects parameter 1 to be object, ...
