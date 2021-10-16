e83b402daff0e310157770229a16ca91f16ce2a8
kumackey <kumak1t09e0@gmail.com>
2021-10-17 00:18:30 +0900

    微修正

--- EmptyArray/map2.php
+++ EmptyArray/map2.php
@@ -1,17 +1,17 @@
 <?php
 
 $map = [
-    'apple' => 'red',
-    'lemon' => 'yellow',
-    'grape' => 'purple',
-    'melon' => 'green',
-    'peach' => 'pink',
+    'even' => [0, 2, 4, 6, 8,],
+    'odd' => [1, 3, 5, 7, 9,],
+    'prime' => [2, 3, 5, 7,],
 ];
 
-// 青色だけを選択
-$filtered_map = array_filter($map, function ($value) {
-    return $value === 'blue';
-});
+echo json_encode($map) . PHP_EOL;
+
+// キーがfibonacciの要素のみを選択。結果は空配列となる。
+$filtered_map = array_filter($map, function ($key) {
+    return $key === 'fibonacci';
+}, ARRAY_FILTER_USE_KEY);
 
 if (empty($filtered_map)) {
     // エンコードするときに空オブジェクトとなる
